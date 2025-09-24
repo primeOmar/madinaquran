1ea8723ad6fceac27e593cba6c66cb3c92113259
primeOmar <133329424+primeOmar@users.noreply.github.com>
2025-09-24 19:38:31 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public', 
+  base: './', 
   build: {
     outDir: 'dist',
-    copyPublicDir: true, 
-    assetsDir: 'assets', 
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   server: {
     port: 3000
