80c9d4bb35170176e291b95277534d9e3536e7e6
primeOmar <133329424+primeOmar@users.noreply.github.com>
2025-10-05 15:37:23 +0300

    Update TeacherDashboard.jsx

--- src/pages/TeacherDashboard.jsx
+++ src/pages/TeacherDashboard.jsx
@@ -9,7 +9,7 @@ import {
 } from "lucide-react";
 import { useAuth } from '../components/AuthContext';
 import { teacherApi } from '../lib/supabaseClient';
-import { teacherApi } from '../lib/teacherApi';
+import { teachervideoApi } from '../lib/teacherApi';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom'; 
 import VideoCall from '../components/VideoCall'
